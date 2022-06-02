6e4b6612813e7bfe242ecc844a37cc6a5a690062
leonard475192 <leonard.t1028@gmail.com>
2022-06-02 16:51:46 +0900

    :+1: feat: seed generate 100 dummy data

--- db/seeds/development/tasks.rb
+++ db/seeds/development/tasks.rb
@@ -1,7 +1,25 @@
+require "faker"
+
 Task.create!(
-  title: "タスク1",
+  title: "タスク",
   status: "DONE",
   content: "あいうえお",
   man_hour: 3,
   deadline_at: Date.tomorrow,
 )
+
+task_status = [
+  "TODO",
+  "DOING",
+  "DONE",
+]
+
+100.times do |n|
+  Task.create!(
+    title: "タスク#{n}",
+    status: task_status[n % 3],
+    content: Faker::Books::Lovecraft.paragraph,
+    man_hour: n % 10,
+    deadline_at: Faker::Date.forward(300),
+  )
+end
