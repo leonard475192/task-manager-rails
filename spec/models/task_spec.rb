155fd0af27804879807f40c8f8dbe3df6bc2f817
leonard475192 <leonard.t1028@gmail.com>
2022-06-10 10:58:05 +0900

    :white_check_mark: test: model task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーション" do
+    example "成功: status=TODO" do
+      task = build(:task)
+      expect(task).to be_valid  
+    end
+
+    example "成功: status=DOING" do
+      task = build(:task, status: "DOING")
+      expect(task).to be_valid  
+    end
+
+    example "成功: status=DONE" do
+      task = build(:task, status: "DONE")
+      expect(task).to be_valid  
+    end
+
+    example "タイトルがないものは無効" do
+      task = build(:task, title: nil)
+      expect(task).not_to be_valid
+    end
+
+    example "タイトルが空文字列は無効" do
+      task = build(:task, title: "")
+      expect(task).not_to be_valid  
+    end
+
+    example "255文字を超えるタイトルは無効" do
+      task = build(:task, title: "a"*256)
+      expect(task).not_to be_valid  
+    end
+
+    example "制約に含まれないステータスは無効" do
+      task = build(:task, status: "")
+      expect(task).not_to be_valid  
+    end
+  end
 end
